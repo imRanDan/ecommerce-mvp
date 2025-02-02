eb0c142d2915e7963470bc52294c86f924b6e544
Danyal Imran <dan.imran97@gmail.com>
2025-02-02 18:16:13 -0500

    add unique categories from api and work on sidebar styling for filters

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -26,16 +26,83 @@ const Sidebar = () => {
       try {
         const response = await fetch("https://dummyjson.com/products");
         const data: FetchResponse = await response.json();
-        console.log(data);
+        // howto get unique categories from api
+        const uniqueCategories = Array.from(
+          new Set(data.products.map((product) => product.category))
+        );
+        setCategories(uniqueCategories);
       } catch (error) {
-        console.error("Error fetching product(s)", error);
+        console.error("Error fetching products", error);
       }
     };
 
     fetchCategories();
   }, []);
 
-  return <div>Sidebar</div>;
+  return (
+    <div className="w-64 p-5 h-screen">
+      <h1 className="text-2xl font-bold mb-10 mt-4">Fools Goods</h1>
+
+      <section>
+        <input
+          type="text"
+          className="border-2 rounded px-2 sm:mb-0"
+          placeholder="Search Product"
+        />
+        <div className="flex justify-center items-center">
+          <input
+            type="text"
+            className="border-2 mr-2 px-5 py-3 mb-3 w-full"
+            placeholder="Min"
+          />
+
+          <input
+            type="text"
+            className="border-2 mr-2 px-5 py-3 mb-3 w-full"
+            placeholder="Max"
+          />
+        </div>
+
+        {/* Categories */}
+        <section>
+          <div className="mb-5">
+            <h2 className="text-xl font-semibold mb-3">Categories</h2>
+          </div>
+
+          {categories.map((category, index) => (
+            <label key={index} className="block mb-2">
+              <input
+                type="radio"
+                name="category"
+                value={category}
+                className="mr-2 w-[16px] h-[16px]"
+              />
+              {category.toLowerCase()}
+            </label>
+          ))}
+        </section>
+        {/* Keywords */}
+
+        <div className="mb-5">
+          <h2 className="text-xl font-semibold mb-3">Keywords</h2>
+          <div>
+            {keywords.map((keyword, index) => (
+              <button
+                key={index}
+                className="block mb-2 px-4 py-2 w-full text-left border rounded hover:bg-gray-200"
+              >
+                {keyword.toLowerCase()}
+              </button>
+            ))}
+          </div>
+        </div>
+
+            <button
+
+
+      </section>
+    </div>
+  );
 };
 
 export default Sidebar;
